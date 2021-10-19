930ea0c9226029d49e8cf11d8efa1087924f7e7b
gousteris <44735249+gousteris@users.noreply.github.com>
2021-10-20 02:04:57 +0300

    Support 'elementAt' for nested values

--- lib/src/translations.dart
+++ lib/src/translations.dart
@@ -8,7 +8,7 @@ class Translations {
 
     /// Try to look it up as a nested key
     if (isNestedKey(key)) {
-      returnValue = getNested(key);
+      returnValue = getNested(key, elementAt);
     }
 
     /// If we failed to find the key as a nested key, then fall back
@@ -18,11 +18,10 @@ class Translations {
     } else {
       returnValue ??= _translations?[key];
     }
-
     return returnValue;
   }
 
-  String? getNested(String key) {
+  String? getNested(String key, int? elementAt) {
     if (isNestedCached(key)) return _nestedKeysCache[key];
 
     final keys = key.split('.');
@@ -34,6 +33,10 @@ class Translations {
 
     for (var i = 1; i < keys.length; i++) {
       if (value is Map<String, dynamic>) value = value[keys[i]];
+    }
+
+    if (elementAt != null) {
+      value = value[elementAt];
     }
 
     /// If we found the value, cache it. If the value is null then
